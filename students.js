8569fa05486594e9445fc1245ccd0521a43e4d6d
MetlerEG7609 <metlereg6@gmail.com>
2026-06-26 06:56:15 +0300

    Add files via upload

---
+++ students.js
@@ -0,0 +1,21 @@
+data = [
+
+    {
+        name: "Ibrahim",
+        address: "egypt",
+        age: 17
+    },
+
+    {
+        name: "Ahmed",
+        address: "egypt",
+        age: 21
+    },
+
+    {
+        name: "Mohamed",
+        address: "egypt",
+        age: 19
+    }
+
+];
